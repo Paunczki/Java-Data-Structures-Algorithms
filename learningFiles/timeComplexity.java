8fab6613afc7cdb9902c43a072c4d8643fa938a6
Paunczki <johnnymikos@gmail.com>
2019-08-14 09:46:14 -0500

    Been a while, but here some work

--- learningFiles/timeComplexity.java
+++ learningFiles/timeComplexity.java
@@ -2,6 +2,11 @@ public class timeComplexity{
     // Time complexity is one of the most important things when it 
     // comes to building an efficient application
 
-    // Time 
+    // The two main factors of efficiency are space and time
+    // You want a fast app, but you do not want to take up too much space
+    // These usually go hand-in-hand, but a quick explanation may help
 
+    public static void main(String[] args){
+        
+    }
 }
